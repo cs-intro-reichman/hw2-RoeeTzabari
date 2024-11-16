1b12d1b1600b729cb6bb58b9e681f2a5f31abc8e
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2024-11-16 02:41:03 +0200

    Completed all the tasks.

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,33 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    
+		int N = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int num, count = 0;
+		boolean isFirst = true;
+
+		for (int i = 1; i <= N; i++) {
+			num = i;
+
+			while (num != 1 || isFirst) {
+				isFirst = false;
+				if (mode.equals("v")) {
+					System.out.print(num + " ");
+				}
+
+				if (num % 2 == 0) num /= 2;
+				else num = num * 3 + 1;
+
+				count++;
+			}
+			if (mode.equals("v")) {
+				System.out.print(1 + " ");
+				System.out.println("(" + (count + 1) + ")");
+			}
+			count = 0;
+		}
+		
+		System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
 	}
 }
