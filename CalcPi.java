e0048faf4b2b49e4693079e94ce942d305c7a3a6
Roee Tzabari <roee.tzabari@post.runi.ac.il>
2024-11-16 03:01:39 +0200

    Fixed outputs.

--- CalcPi.java
+++ CalcPi.java
@@ -13,6 +13,6 @@ public class CalcPi {
 		}
 		sum *= 4.0;
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + sum);
+		System.out.println("pi, approximated:     " + sum);
 	}
 }
